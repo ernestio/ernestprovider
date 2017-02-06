6afe8ff85ccb6e649b75b5b21f5dcf5920ab9ffa
Adrià Cidre <adria.cidre@gmail.com>
2017-02-06 15:14:50 +0100

    TF Subnet

--- providers/azure/subnet/entity.go
+++ providers/azure/subnet/entity.go
@@ -7,17 +7,20 @@ package subnet
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 
-	"github.com/ernestio/ernestprovider/event"
-	"github.com/ernestio/ernestprovider/providers/azure"
+	"github.com/hashicorp/terraform/builtin/providers/azurerm"
+	"github.com/hashicorp/terraform/helper/schema"
 
-	"github.com/Azure/azure-sdk-for-go/arm/network"
+	aes "github.com/ernestio/crypto/aes"
+	"github.com/ernestio/ernestprovider/event"
 )
 
 // Event : This is the Ernest representation of an azure subnet
 type Event struct {
 	event.Base
+
 	ID                   string   `json:"id"`
 	Name                 string   `json:"name" validate:"required"`
 	ResourceGroupName    string   `json:"resource_group_name" validate:"required"`
@@ -33,6 +36,10 @@ type Event struct {
 	SubscriptionID string `json:"azure_subscription_id"`
 	Environment    string `json:"environment"`
 
+	Provider     *schema.Provider
+	Component    *schema.Resource
+	ResourceData *schema.ResourceData
+	Schema       map[string]*schema.Schema
 	ErrorMessage string           `json:"error,omitempty"`
 	Subject      string           `json:"-"`
 	Body         []byte           `json:"-"`
@@ -40,27 +47,23 @@ type Event struct {
 	Validator    *event.Validator `json:"-"`
 }
 
-type subnet struct {
-	Name          string `json:"name" validate:"required"`
-	AddressPrefix string `json:"address_prefix" validate:"required,cidr"`
-	SecurityGroup string `json:"security_group"`
-}
-
 // New : Constructor
-func New(subject string, body []byte, cryptoKey string, val *event.Validator) event.Event {
+func New(subject string, body []byte, cryptoKey string, val *event.Validator) (event.Event, error) {
+	var err error
 	n := Event{Subject: subject, Body: body, CryptoKey: cryptoKey, Validator: val}
-
-	return &n
-}
-
-// Azure subnet client
-func (ev *Event) client() *network.SubnetsClient {
-	client, err := azure.Provider(ev.SubscriptionID, ev.ClientID, ev.ClientSecret, ev.TenantID, ev.Environment, ev.CryptoKey)
-	if err != nil {
-		panic(err)
+	n.Provider = azurerm.Provider().(*schema.Provider)
+	n.Component = n.Provider.ResourcesMap["azurerm_subnet"]
+	n.Schema = n.schema()
+	n.Body = body
+	n.Subject = subject
+	n.CryptoKey = cryptoKey
+	n.Validator = val
+	if n.ResourceData, err = n.toResourceData(body); err != nil {
+		n.Log("error", err.Error())
+		return &n, err
 	}
 
-	return &client.SubnetClient
+	return &n, nil
 }
 
 // Validate checks if all criteria are met
@@ -71,6 +74,71 @@ func (ev *Event) Validate() error {
 // Find : Find an object on azure
 func (ev *Event) Find() error {
 	return errors.New(ev.Subject + " not supported")
+}
+
+// Create : Creates a Virtual Network on Azure using terraform
+// providers
+func (ev *Event) Create() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Create(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error creating the requestd resource : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// Update : Updates an existing Virtual Network on Azure
+// by using azurerm terraform provider resource
+func (ev *Event) Update() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Update(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error creating the requestd resource : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// Get : Requests and loads the resource to Azure through azurerm
+// terraform provider
+func (ev *Event) Get() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Read(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error getting virtual network : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	ev.toEvent()
+	return nil
+}
+
+// Delete : Deletes the received resource from azure through
+// azurerm terraform provider
+func (ev *Event) Delete() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Delete(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error deleting the requested resource : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	return nil
 }
 
 // GetBody : Gets the body for this event
@@ -103,4 +171,93 @@ func (ev *Event) Error(err error) {
 	ev.ErrorMessage = err.Error()
 
 	ev.Body, err = json.Marshal(ev)
+}
+
+// Translates a ResourceData on a valid Ernest Event
+func (ev *Event) toEvent() {
+	ev.Name = ev.ResourceData.Get("name").(string)
+	ev.ResourceGroupName = ev.ResourceData.Get("resource_group_name").(string)
+	ev.VirtualNetworkName = ev.ResourceData.Get("virtual_network_name").(string)
+	ev.AddressPrefix = ev.ResourceData.Get("address_prefix").(string)
+	ev.NetworkSecurityGroup = ev.ResourceData.Get("network_security_group").(string)
+	ev.RouteTable = ev.ResourceData.Get("route_table_id").(string)
+	ev.IPConfigurations = ev.ResourceData.Get("ip_configurations").([]string)
+}
+
+// Translates the current event on a valid ResourceData
+func (ev *Event) toResourceData(body []byte) (*schema.ResourceData, error) {
+	var d schema.ResourceData
+	d.SetSchema(ev.Schema)
+	if err := json.Unmarshal(body, &ev); err != nil {
+		err := fmt.Errorf("Error on input message : %s", err)
+		ev.Log("error", err.Error())
+		return nil, err
+	}
+
+	crypto := aes.New()
+
+	encFields := make(map[string]string)
+	encFields["subscription_id"] = ev.SubscriptionID
+	encFields["client_id"] = ev.ClientID
+	encFields["client_secret"] = ev.ClientSecret
+	encFields["tenant_id"] = ev.TenantID
+	encFields["environment"] = ev.Environment
+	for k, v := range encFields {
+		dec, err := crypto.Decrypt(v, ev.CryptoKey)
+		if err != nil {
+			err := fmt.Errorf("Field '%s' not valid : %s", k, err)
+			ev.Log("error", err.Error())
+			return nil, err
+		}
+		if err := d.Set(k, dec); err != nil {
+			err := fmt.Errorf("Field '%s' not valid : %s", k, err)
+			ev.Log("error", err.Error())
+			return nil, err
+		}
+	}
+
+	fields := make(map[string]interface{})
+	fields["name"] = ev.Name
+	fields["resource_group_name"] = ev.ResourceGroupName
+	fields["virtual_network_name"] = ev.VirtualNetworkName
+	fields["address_prefix"] = ev.AddressPrefix
+	fields["network_security_group"] = ev.NetworkSecurityGroup
+	fields["route_table_id"] = ev.RouteTable
+	fields["ip_configurations"] = ev.IPConfigurations
+	for k, v := range fields {
+		if err := d.Set(k, v); err != nil {
+			err := fmt.Errorf("Field '%s' not valid : %s", k, err)
+			ev.Log("error", err.Error())
+			return nil, err
+		}
+	}
+
+	return &d, nil
+}
+
+// Based on the Provider and Component schemas it calculates
+// the necessary schema to be create a new ResourceData
+func (ev *Event) schema() (sch map[string]*schema.Schema) {
+	if ev.Schema != nil {
+		return ev.Schema
+	}
+	a := ev.Provider.Schema
+	b := ev.Component.Schema
+	sch = a
+	for k, v := range b {
+		sch[k] = v
+	}
+	return sch
+}
+
+// Azure virtual network client
+func (ev *Event) client() (*azurerm.ArmClient, error) {
+	client, err := ev.Provider.ConfigureFunc(ev.ResourceData)
+	if err != nil {
+		err := fmt.Errorf("Can't connect to provider : %s", err)
+		ev.Log("error", err.Error())
+		return nil, err
+	}
+	c := client.(*azurerm.ArmClient)
+	return c, nil
 }
