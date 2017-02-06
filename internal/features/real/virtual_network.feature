1545c8263221f46fdd7ea0a0bd594ea8b6a33692
Adrià Cidre <adria.cidre@gmail.com>
2017-02-06 14:02:45 +0100

    TF

--- internal/features/real/virtual_network.feature
+++ internal/features/real/virtual_network.feature
@@ -4,7 +4,9 @@ Feature: Virtual network
 
   Scenario: Creting a virtual network
     Given I have no messages on the buffer
-    And I request "azure_virtualnetwork.delete.azure" with "vn_create.json"
+    #And I request "azure_virtualnetwork.delete.azure" with "vn_create.json"
+    #And I request "azure_resource_group.create.azure" with "rg_create.json"
+    #And I request "azure_subnet.create.azure" with "sub_create.json"
     When I request "azure_virtualnetwork.create.azure" with "vn_create.json"
     Then I should get a "azure_virtualnetwork.create.azure.done" response with "name" as "testing_resource"
     And I should get a "azure_virtualnetwork.create.azure.done" response with "address_space.0" as "10.1.2.0/24"
@@ -16,58 +18,3 @@ Feature: Virtual network
     And I should get a "azure_virtualnetwork.create.azure.done" response with "resource_group_name" as "ernest"
     And I should get a "azure_virtualnetwork.create.azure.done" response with "tags.t1" as "one"
     And I should get a "azure_virtualnetwork.create.azure.done" response with "tags.t2" as "two"
-
-  Scenario: Updating a virtual network
-    Given I have no messages on the buffer
-    And testing "azure_virtualnetwork" does not exist
-    And I request "azure_virtualnetwork.create.azure" with "vn_update.json"
-    And I have no messages on the buffer
-    When I request "azure_virtualnetwork.update.azure" with "vn_update.json"
-    Then I should get a "azure_virtualnetwork.update.azure.done" response with "name" as "testing_resource"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "address_space.0" as "10.1.2.0/24"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "address_space.1" as "10.1.3.0/24"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "dns_server_names.0" as "8.8.8.8"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "dns_server_names.1" as "4.4.4.4"
-    And I should get a "azure_virtualnetwork.udpate.azure.done" response with "subnets.0.name" as "subnet1"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "subnets.0.address_prefix" as "10.1.2.0/24"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "location" as "westus"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "resource_group_name" as "ernest"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "tags.t1" as "one"
-    And I should get a "azure_virtualnetwork.update.azure.done" response with "tags.t2" as "three"
-
-  Scenario: Retrieve a virtual network
-    Given I have no messages on the buffer
-    And testing "azure_virtualnetwork" does not exist
-    And I request "azure_virtualnetwork.create.azure" with "vn_update.json"
-    And I have no messages on the buffer
-    When I request "azure_virtualnetwork.get.azure" with "vn_get.json"
-    Then I should get a "azure_virtualnetwork.get.azure.done" response with "name" as "testing_resource"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "address_space.0" as "10.1.2.0/24"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "address_space.1" as "10.1.3.0/24"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "dns_server_names.0" as "8.8.8.8"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "dns_server_names.1" as "4.4.4.4"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "subnets.0.name" as "subnet1"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "subnets.0.address_prefix" as "10.1.2.0/24"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "location" as "westus"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "resource_group_name" as "ernest"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "tags.t1" as "one"
-    And I should get a "azure_virtualnetwork.get.azure.done" response with "tags.t2" as "three"
-
-
-  Scenario: Removing a virtual network
-    Given I have no messages on the buffer
-    And testing "azure_virtualnetwork" does not exist
-    And I request "azure_virtualnetwork.create.azure" with "vn_create.json"
-    And I have no messages on the buffer
-    When I request "azure_virtualnetwork.delete.azure" with "vn_create.json"
-    Then I should get a "azure_virtualnetwork.delete.azure.done" response with "name" as "testing_resource"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "address_space.0" as "10.1.2.0/24"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "dns_server_names.0" as "8.8.8.8"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "dns_server_names.1" as "4.4.4.4"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "subnets.0.name" as "subnet1"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "subnets.0.address_prefix" as "10.1.2.0/24"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "location" as "westus"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "resource_group_name" as "ernest"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "tags.t1" as "one"
-    And I should get a "azure_virtualnetwork.delete.azure.done" response with "tags.t2" as "two"
-
--- providers/azure/virtualnetwork/entity.go
+++ providers/azure/virtualnetwork/entity.go
@@ -7,32 +7,40 @@ package virtualnetwork
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 
-	"github.com/ernestio/ernestprovider/event"
-	"github.com/ernestio/ernestprovider/providers/azure"
+	"github.com/hashicorp/terraform/builtin/providers/azurerm"
+	"github.com/hashicorp/terraform/helper/hashcode"
+	"github.com/hashicorp/terraform/helper/schema"
 
-	"github.com/Azure/azure-sdk-for-go/arm/network"
+	aes "github.com/ernestio/crypto/aes"
+	"github.com/ernestio/ernestprovider/event"
 )
 
 // Event : This is the Ernest representation of an azure virtual
 //         network
 type Event struct {
 	event.Base
-	ID                string             `json:"id"`
-	Name              string             `json:"name" validate:"required"`
-	AddressSpace      []string           `json:"address_space" validate:"min=1"`
-	DNSServerNames    []string           `json:"dns_server_names" validate:"dive,ip"`
-	Subnets           []subnet           `json:"subnets" validate:"min=1,dive"`
-	Location          string             `json:"location"`
-	ClientID          string             `json:"azure_client_id"`
-	ClientSecret      string             `json:"azure_client_secret"`
-	TenantID          string             `json:"azure_tenant_id"`
-	SubscriptionID    string             `json:"azure_subscription_id"`
-	Environment       string             `json:"environment"`
-	ResourceGroupName string             `json:"resource_group_name"`
-	Tags              map[string]*string `json:"tags"`
 
+	ID                string            `json:"id"`
+	Name              string            `json:"name" validate:"required"`
+	AddressSpace      []string          `json:"address_space" validate:"min=1"`
+	DNSServerNames    []string          `json:"dns_server_names" validate:"dive,ip"`
+	Subnets           []subnet          `json:"subnets" validate:"min=1"`
+	Location          string            `json:"location"`
+	ClientID          string            `json:"azure_client_id"`
+	ClientSecret      string            `json:"azure_client_secret"`
+	TenantID          string            `json:"azure_tenant_id"`
+	SubscriptionID    string            `json:"azure_subscription_id"`
+	Environment       string            `json:"environment"`
+	ResourceGroupName string            `json:"resource_group_name"`
+	Tags              map[string]string `json:"tags"`
+
+	Provider     *schema.Provider
+	Component    *schema.Resource
+	ResourceData *schema.ResourceData
+	Schema       map[string]*schema.Schema
 	ErrorMessage string           `json:"error,omitempty"`
 	Subject      string           `json:"-"`
 	Body         []byte           `json:"-"`
@@ -41,29 +49,33 @@ type Event struct {
 }
 
 type subnet struct {
-	Name          string `json:"name" validate:"required"`
-	AddressPrefix string `json:"address_prefix" validate:"required,cidr"`
+	Name          string `json:"name"`
+	AddressPrefix string `json:"address_prefix"`
 	SecurityGroup string `json:"security_group"`
 }
 
-// New : Constructor
-func New(subject string, body []byte, cryptoKey string, val *event.Validator) event.Event {
+// New : Virtual Network constructor returning an Event object
+// and an error in case something fails
+func New(subject string, body []byte, cryptoKey string, val *event.Validator) (event.Event, error) {
+	var err error
 	n := Event{Subject: subject, Body: body, CryptoKey: cryptoKey, Validator: val}
-
-	return &n
-}
-
-// Azure virtual network client
-func (ev *Event) client() *network.VirtualNetworksClient {
-	client, err := azure.Provider(ev.SubscriptionID, ev.ClientID, ev.ClientSecret, ev.TenantID, ev.Environment, ev.CryptoKey)
-	if err != nil {
-		panic(err)
+	n.Provider = azurerm.Provider().(*schema.Provider)
+	n.Component = n.Provider.ResourcesMap["azurerm_virtual_network"]
+	n.Schema = n.schema()
+	n.Body = body
+	n.Subject = subject
+	n.CryptoKey = cryptoKey
+	n.Validator = val
+	if n.ResourceData, err = n.toResourceData(body); err != nil {
+		n.Log("error", err.Error())
+		return &n, err
 	}
 
-	return &client.VnetClient
+	return &n, nil
 }
 
-// Validate checks if all criteria are met
+// Validate : checks if all event fields are valid, it
+// responds with an error in case something is wrong
 func (ev *Event) Validate() error {
 	return ev.Validator.Validate(ev)
 }
@@ -73,9 +85,69 @@ func (ev *Event) Find() error {
 	return errors.New(ev.Subject + " not supported")
 }
 
-// Update : Updates a nat object on azure
+// Create : Creates a Virtual Network on Azure using terraform
+// providers
+func (ev *Event) Create() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Create(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error creating the requestd resource : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// Update : Updates an existing Virtual Network on Azure
+// by using azurerm terraform provider resource
 func (ev *Event) Update() error {
-	return ev.Create()
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Update(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error creating the requestd resource : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+// Get : Requests and loads the resource to Azure through azurerm
+// terraform provider
+func (ev *Event) Get() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Read(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error getting virtual network : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	ev.toEvent()
+	return nil
+}
+
+// Delete : Deletes the received resource from azure through
+// azurerm terraform provider
+func (ev *Event) Delete() error {
+	c, err := ev.client()
+	if err != nil {
+		return err
+	}
+	if err := ev.Component.Delete(ev.ResourceData, c); err != nil {
+		err := fmt.Errorf("Error deleting the requested resource : %s", err)
+		ev.Log("error", err.Error())
+		return err
+	}
+
+	return nil
 }
 
 // GetBody : Gets the body for this event
@@ -110,42 +182,113 @@ func (ev *Event) Error(err error) {
 	ev.Body, err = json.Marshal(ev)
 }
 
-func (ev *Event) getVirtualNetworkProperties() *network.VirtualNetworkPropertiesFormat {
-	// first; get address space prefixes:
-	prefixes := ev.AddressSpace
+// Translates a ResourceData on a valid Ernest Event
+func (ev *Event) toEvent() {
+	ev.Name = ev.ResourceData.Get("name").(string)
+	ev.AddressSpace = ev.ResourceData.Get("address_space").([]string)
+	ev.DNSServerNames = ev.ResourceData.Get("dns_servers").([]string)
+	ev.Location = ev.ResourceData.Get("location").(string)
+	ev.Subnets = ev.ResourceData.Get("subnet").([]subnet)
+	ev.Tags = ev.ResourceData.Get("tags").(map[string]string)
+}
 
-	// then; the dns servers:
-	dnses := ev.DNSServerNames
+// Translates the current event on a valid ResourceData
+func (ev *Event) toResourceData(body []byte) (*schema.ResourceData, error) {
+	var d schema.ResourceData
+	d.SetSchema(ev.Schema)
+	if err := json.Unmarshal(body, &ev); err != nil {
+		err := fmt.Errorf("Error on input message : %s", err)
+		ev.Log("error", err.Error())
+		return nil, err
+	}
 
-	// then; the subnets:
-	subnets := []network.Subnet{}
-	for _, subnet := range ev.Subnets {
-		name := subnet.Name
-		prefix := subnet.AddressPrefix
-		secGroup := subnet.SecurityGroup
+	crypto := aes.New()
 
-		var subnetObj network.Subnet
-		subnetObj.Name = &name
-		subnetObj.SubnetPropertiesFormat = &network.SubnetPropertiesFormat{}
-		subnetObj.SubnetPropertiesFormat.AddressPrefix = &prefix
-
-		if secGroup != "" {
-			subnetObj.SubnetPropertiesFormat.NetworkSecurityGroup = &network.SecurityGroup{
-				ID: &secGroup,
-			}
+	encFields := make(map[string]string)
+	encFields["subscription_id"] = ev.SubscriptionID
+	encFields["client_id"] = ev.ClientID
+	encFields["client_secret"] = ev.ClientSecret
+	encFields["tenant_id"] = ev.TenantID
+	encFields["environment"] = ev.Environment
+	for k, v := range encFields {
+		dec, err := crypto.Decrypt(v, ev.CryptoKey)
+		if err != nil {
+			err := fmt.Errorf("Field '%s' not valid : %s", k, err)
+			ev.Log("error", err.Error())
+			return nil, err
 		}
-
-		subnets = append(subnets, subnetObj)
+		if err := d.Set(k, dec); err != nil {
+			err := fmt.Errorf("Field '%s' not valid : %s", k, err)
+			ev.Log("error", err.Error())
+			return nil, err
+		}
 	}
 
-	// finally; return the struct:
-	return &network.VirtualNetworkPropertiesFormat{
-		AddressSpace: &network.AddressSpace{
-			AddressPrefixes: &prefixes,
-		},
-		DhcpOptions: &network.DhcpOptions{
-			DNSServers: &dnses,
-		},
-		Subnets: &subnets,
+	fields := make(map[string]interface{})
+	fields["name"] = ev.Name
+	fields["address_space"] = ev.AddressSpace
+	fields["dns_servers"] = ev.DNSServerNames
+	fields["location"] = ev.Location
+	fields["subnet"] = ev.mapSubnets()
+	fields["tags"] = ev.Tags
+	for k, v := range fields {
+		if err := d.Set(k, v); err != nil {
+			err := fmt.Errorf("Field '%s' not valid : %s", k, err)
+			ev.Log("error", err.Error())
+			return nil, err
+		}
 	}
+
+	return &d, nil
+}
+
+// Based on the Provider and Component schemas it calculates
+// the necessary schema to be create a new ResourceData
+func (ev *Event) schema() (sch map[string]*schema.Schema) {
+	if ev.Schema != nil {
+		return ev.Schema
+	}
+	a := ev.Provider.Schema
+	b := ev.Component.Schema
+	sch = a
+	for k, v := range b {
+		sch[k] = v
+	}
+	return sch
+}
+
+// Azure virtual network client
+func (ev *Event) client() (*azurerm.ArmClient, error) {
+	client, err := ev.Provider.ConfigureFunc(ev.ResourceData)
+	if err != nil {
+		err := fmt.Errorf("Can't connect to provider : %s", err)
+		ev.Log("error", err.Error())
+		return nil, err
+	}
+	c := client.(*azurerm.ArmClient)
+	return c, nil
+}
+
+// Maps the event subnets to a valid ResourceData List
+func (ev *Event) mapSubnets() *schema.Set {
+	list := &schema.Set{
+		F: resourceAzureSubnetHash,
+	}
+	for _, sub := range ev.Subnets {
+		s := map[string]interface{}{}
+		s["name"] = sub.Name
+		s["address_prefix"] = sub.AddressPrefix
+		list.Add(s)
+	}
+	return list
+}
+
+func resourceAzureSubnetHash(v interface{}) int {
+	m := v.(map[string]interface{})
+	subnet := m["name"].(string) + m["address_prefix"].(string)
+	if securityGroup, present := m["security_group"]; present {
+		subnet = subnet + securityGroup.(string)
+	}
+
+	return hashcode.String(subnet)
 }
